8694b724baa851ffb1c353a52c40f3db2624d9a6
Fadi Hassanieh <fadihassanieh@live.com>
2023-04-08 19:00:16 +0300

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,9 @@ import Apps from "./Pages/Apps";
 import Books from "./Pages/Books";
 import Games from "./Pages/Games";
 import Movies from "./Pages/Movies";
-import { useAppDispatch } from "./hooks";
-import { getMoviesAsync } from "./features/moviesSlice";
 import RouteTo from "./data/Routes";
 
 function App() {
-	const dispatch = useAppDispatch();
-	const getMovies = async () => {
-		const movies = await dispatch(getMoviesAsync());
-		console.log(movies);
-	};
-	useEffect(() => {
-		getMovies();
-	}, []);
-
 	return (
 		<Routes>
 			<Route path="/" element={<NavBar />}>
