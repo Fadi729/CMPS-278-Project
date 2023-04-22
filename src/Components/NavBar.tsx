5905e9c0e02b7317fd9e3b4ee1b22f87aa133604
tala-cyber <tha23@mail.aub.edu>
2023-04-23 02:02:32 +0300

    CSSing

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -21,8 +21,8 @@ const NavBar = () => {
 				id="navbar"
 			>
 				<nav className="flex w-full h-full items-center font-google bg-white">
-					<div className="flex mr-3 ml-7 w-[22.5%]">
-						<svg className="h-10 w-10 mr-2" aria-hidden="true" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+					<div className="flex mr-3 ml-7 w-[24%]" style={{marginLeft:'26px'}}>
+						<svg className="h-10 w-11 mr-2" aria-hidden="true" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
 							<path fill="none" d="M0,0h40v40H0V0z"></path>
 							<g>
 								<path d="M19.7,19.2L4.3,35.3c0,0,0,0,0,0c0.5,1.7,2.1,3,4,3c0.8,0,1.5-0.2,2.1-0.6l0,0l17.4-9.9L19.7,19.2z" fill="#EA4335"></path>
@@ -34,9 +34,9 @@ const NavBar = () => {
 								<path d="M19.8,20l8-7.9L10.5,2.3C9.9,1.9,9.1,1.7,8.3,1.7c-1.9,0-3.6,1.3-4,3c0,0,0,0,0,0L19.8,20z" fill="#34A853"></path>
 							</g>
 						</svg>
-						<span className="font-medium leading-7 tracking-[0] text-[1.375rem] text-[#5f6368] self-center">google_logo Play</span>
+						<span className="font-medium leading-7 tracking-[0] text-[1.375rem] text-[#5f6368]  self-center" style={{width: '139px', fontSize:'22.2px', marginTop:'-1px'}}>google_logo Play</span>
 					</div>
-					<div className="flex justify-start gap-10 text-sm w-full">
+					<div className="flex justify-start gap-8 text-[14.5px] w-full text-[#5f6368] mt-1">
 						<NavLink
 							to={RouteTo.Games}
 							className={({ isActive }) => {
