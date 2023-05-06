ca6fc386d254c395be1a6d44cb0d72aae34298f1
Majd Fakih <mhf28@mail.aub.edu>
2023-05-06 16:42:41 +0300

    footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,36 +1,56 @@
 import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import RouteTo from "../data/Routes";
 
 import axios from 'axios'
 
 const Foot = () => {
-    let country:string = "";
-    
-    axios.get('https://ipapi.co/json/').then((response) => {
-        let data = response.data;
-        country = data.country_name;
-    });
+	const [state, setState] = useState({
+		countryName: "",
+	  });
+	
+	  const getGeoInfo = () => {
+		axios
+		  .get("https://ipapi.co/json/")
+		  .then((response) => {
+			let data = response.data;
+			setState({
+			  ...state,
+			  countryName: data.country_name,
+			});
+		  })
+		  .catch((error) => {
+			console.log(error);
+		  });
+	  };
+	
+	  useEffect(() => {
+		getGeoInfo();
+	  }, []);
+	
+
+
 	return (
 		<>
         <footer>
-					<div className="flex justify-start gap-10 text-sm w-full m-7 mb-0">
+					<div className="flex justify-between gap-10 text-sm w-full m-7 mb-0 ml-0">
 						<NavLink
 							to={RouteTo.ToS}
 						>
-							<div className="flex flex-col justify-center">
+							<div className="flex flex-col justify-between">
 								<span className="my-[1.3rem]">Terms of service</span>
 							</div>
 						</NavLink>
 						<NavLink
 							to={RouteTo.About}
 						>
-							<div className="flex flex-col justify-center">
+							<div className="flex flex-col justify-between">
 								<span className="my-[1.3rem]">About</span>
                                 </div>
 						</NavLink>
 
-                        <div className="flex flex-col justify-center">
-							<span className="my-[1.3rem]">{country}</span>
+                        <div className="flex flex-col justify-between">
+							<span className="my-[1.3rem]">Country: {state.countryName}</span>
                         </div>
 
 
